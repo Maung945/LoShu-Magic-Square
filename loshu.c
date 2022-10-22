84bb78b7cf54f8bf4d803ad95bcdd45b4612e4b9
Maung945 <myoaung@cpp.edu>
2022-10-21 17:26:35 -0700

    First commit

---
+++ loshu.c
@@ -0,0 +1,7 @@
+/*
+*   Myo Aung- Loshu Magic Square - CS 2600-System Programming
+*   Two function requred to solve this problem
+*   displaySquare() function which will display the the quare
+*   checkIsLoshi() function checks if the square generated randomly by random numbers is a truely Loshi Magic Square.
+*   
+*/
